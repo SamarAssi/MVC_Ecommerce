132e72f319e0f48e24ece37163f6f6a75ba25a64
SamarAssi <79410649+SamarAssi@users.noreply.github.com>
2026-06-03 04:29:33 +0300

    Add Initial/Seed Data

--- Ecommerce/Data/ApplicationDbContext.cs
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -6,9 +6,30 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
         optionsBuilder.UseSqlServer("Server=localhost,1433;Database=EcommerceDb;User Id=sa;Password=Samar@2452;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Mobiles"},
+            new Category { Id = 2, Name = "Tablets"},
+            new Category { Id = 3, Name = "Laptops"}
+        );
+
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "iPhone 13", Description = "Latest iPhone model", Price = 999.99m, Rate = 4.5, CategoryId = 1},
+            new Product { Id = 2, Name = "Samsung Galaxy S21", Description = "Flagship Samsung phone", Price = 899.99m, Rate = 4.3, CategoryId = 1},
+            new Product { Id = 3, Name = "iPad Pro", Description = "Powerful tablet from Apple", Price = 799.99m, Rate = 4.6, CategoryId = 2},
+            new Product { Id = 4, Name = "Microsoft Surface Pro", Description = "Versatile tablet from Microsoft", Price = 899.99m, Rate = 4.4, CategoryId = 2},
+            new Product { Id = 5, Name = "MacBook Pro", Description = "High-performance laptop from Apple", Price = 1299.99m, Rate = 4.7, CategoryId = 3},
+            new Product { Id = 6, Name = "Dell XPS 13", Description = "Compact and powerful laptop from Dell", Price = 999.99m, Rate = 4.5, CategoryId = 3}
+        );
     }
 }
---
+++ Ecommerce/Migrations/20260603012146_SeedData.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Mobiles" },
+                    { 2, "Tablets" },
+                    { 3, "Laptops" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "Name", "Price", "Rate" },
+                values: new object[,]
+                {
+                    { 1, 1, "Latest iPhone model", "iPhone 13", 999.99m, 4.5 },
+                    { 2, 1, "Flagship Samsung phone", "Samsung Galaxy S21", 899.99m, 4.2999999999999998 },
+                    { 3, 2, "Powerful tablet from Apple", "iPad Pro", 799.99m, 4.5999999999999996 },
+                    { 4, 2, "Versatile tablet from Microsoft", "Microsoft Surface Pro", 899.99m, 4.4000000000000004 },
+                    { 5, 3, "High-performance laptop from Apple", "MacBook Pro", 1299.99m, 4.7000000000000002 },
+                    { 6, 3, "Compact and powerful laptop from Dell", "Dell XPS 13", 999.99m, 4.5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
