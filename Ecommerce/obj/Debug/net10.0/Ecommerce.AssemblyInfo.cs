99a899fa0fc47d42e370546a4bf3afaa372ac3b6
SamarAssi <79410649+SamarAssi@users.noreply.github.com>
2026-06-11 13:45:50 +0300

    Split admin's layout into partial views

--- Ecommerce/obj/Debug/net10.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net10.0/Ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0326a60c7a3ea447a9e2a7efcabf5cb9dcc414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f7c9ac49ce342f442ae092d2a77898fc110fe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
